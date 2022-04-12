2c74cc5bc66a7aadd0758cbdf1d9b4ac8d2d6de6
Daniel Collins <dptc98@gmail.com>
2022-04-12 20:29:31 +0100

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "trailingComma": "es5",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "printWidth": 120,
+  "jsxSingleQuote": false,
+  "bracketSpacing": true,
+  "bracketSameLine": true,
+  "arrowParens": "avoid",
+  "htmlWhitespaceSensitivity": "css"
+}
